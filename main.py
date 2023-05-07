c220c6ffa12f5a4efdc1fb273a583e0769084bcd
Hojat <hojat.jaffary@gmail.com>
2023-05-07 13:24:55 -0400

    Starting

---
+++ main.py
@@ -0,0 +1,24 @@
+def get_number(prompt):
+    while True:
+        try:
+            num = float(input(prompt + " "))
+            break
+        except ValueError:
+            print("Invalid input. Try to provide a valid number.")
+    return num
+
+
+def get_operator(prompt):
+    while True:
+        operator = input(prompt)
+        if operator in ('*', '/', '-', '+'):
+            break
+        else:
+            print("You may only enter one of the following operators: + − ∗ /")
+    return operator
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    get_number("Please enter a number:")
+    get_operator("Please enter an operator:")
